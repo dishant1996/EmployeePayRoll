3aead4fd8718cf4b43671b600ff70d13587c03cd
dishant1996 <dishantyashwante1996.com>
2022-10-21 23:09:32 +0530

    added UC1

--- EmpPayroll/EmpPayroll/Program.cs
+++ EmpPayroll/EmpPayroll/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Uc1
+        Random r = new Random();
+        int value = r.Next(0, 2);
+
+        if (value == 0)
+        {
+            Console.WriteLine("emp is present");
+        }
+        else
+        {
+            Console.WriteLine("emp is absent");
+
+        }
+    }
+}
+
